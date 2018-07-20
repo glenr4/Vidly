36401841b6619e64947e2e56a7ef481bd48ed0cc
glenr4 <glenrutherford4@gmail.com>
2018-07-20 14:00:13 +1000

    Customer Details done

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+	public class CustomersController : Controller
+	{
+		List<Customer> customers;
+
+		public CustomersController() {
+			customers = new List<Customer> {
+				new Customer { Id = 1, Name = "John Smith"},
+				new Customer { Id = 2, Name = "Mary Williams"}
+			};
+		}
+
+
+		// GET: Customers
+		public ActionResult Index()
+		{
+			return View(customers);
+		}
+
+		[Route("Customers/Details/{id}")]
+		public ActionResult GetCustomerDetail(int id) {
+			return View("Detail", customers.Find(x => x.Id == id));
+		}
+	}
+}
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -5,6 +5,7 @@ using System.Web;
 
 namespace Vidly.Models {
 	public class Customer {
+		public int Id { get; set; }
 		public string Name { get; set; }
 
 	}
