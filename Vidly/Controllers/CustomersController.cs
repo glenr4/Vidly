130bdbeb027d0a16a686bd753d46df751d67ad42
glenr4 <glenrutherford4@gmail.com>
2018-07-21 17:12:10 +1000

    Migration ApplyAnnotationsToCustomerName

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -9,23 +9,28 @@ namespace Vidly.Controllers
 {
 	public class CustomersController : Controller
 	{
-		List<Customer> customers = new List<Customer>();
+		private List<Customer> _customers = new List<Customer>();
 
 		public CustomersController() {
-			customers.Add(new Customer { Id = 1, Name = "John Smith" });
-			customers.Add(new Customer { Id = 2, Name = "Mary Williams" });
+			_customers.Add(new Customer { Id = 1, Name = "John Smith" });
+			_customers.Add(new Customer { Id = 2, Name = "Mary Williams" });
 		}
 
 
 		// GET: Customers
 		public ActionResult Index()
 		{
-			return View(customers);
+			return View(_customers);
 		}
 
 		[Route("Customers/Details/{id}")]
 		public ActionResult GetDetail(int id) {
-			return View("Detail", customers.Find(x => x.Id == id));
+			var customer = _customers.Find(x => x.Id == id);
+			if(customer == null) {
+				return new HttpNotFoundResult();
+			}
+
+			return View("Detail", customer);
 		}
 	}
 }
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -8,26 +8,30 @@ using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
-	public class MoviesController : Controller
-	{
-		List<Movie> movies = new List<Movie>();
-		List<Customer> customers = new List<Customer>();
+	public class MoviesController : Controller {
+		private List<Movie> _movies = new List<Movie>();
+		private List<Customer> _customers = new List<Customer>();
 
 		public MoviesController() {
-			movies.Add(new Movie { Id = 1, Name = "Shrek" });
-			movies.Add(new Movie { Id = 2, Name = "Wall-E" });
+			_movies.Add(new Movie { Id = 1, Name = "Shrek" });
+			_movies.Add(new Movie { Id = 2, Name = "Wall-E" });
 
 
-			customers.Add(new Customer { Name = "Customer 1" });
-			customers.Add(new Customer { Name = "Customer 2" });
+			_customers.Add(new Customer { Name = "Customer 1" });
+			_customers.Add(new Customer { Name = "Customer 2" });
 		}
 
 		// GET: Movies
-		public ActionResult Random()
-		{
-			var viewModel = new RandomMovieViewModel{
-				Movie = movies.Find(x => x.Id == 1),
-				Customers = customers
+		public ActionResult Random() {
+			var movie = _movies.Find(x => x.Id == 1);
+
+			if (movie == null || _customers == null) {
+				return new HttpNotFoundResult();
+			}
+
+			var viewModel = new RandomMovieViewModel {
+				Movie = movie,
+				Customers = _customers
 			};
 
 			return View(viewModel);
@@ -60,7 +64,7 @@ namespace Vidly.Controllers
 		//	return Content($"pageIndex={pageIndex} & sortBy={sortBy}");
 		//}
 		public ActionResult Index() {
-			return View(movies);
+			return View(_movies);
 		}
 
 		[Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
@@ -70,7 +74,12 @@ namespace Vidly.Controllers
 
 		[Route("Movies/Details/{id}")]
 		public ActionResult GetDetail(int id) {
-			return View("Detail", movies.Find(x => x.Id == id));
+			var movie = _movies.Find(x => x.Id == id);
+			if (movie == null) {
+				return new HttpNotFoundResult();
+			}
+
+			return View("Detail", movie);
 		}
 	}
 }
