4992933c449ca31c72d594da7c91bc8e78b1b827
glenr4 <glenrutherford4@gmail.com>
2018-07-21 16:37:37 +1000

    Populated MembershipTypes

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -7,6 +7,9 @@ namespace Vidly.Models {
 	public class Customer {
 		public int Id { get; set; }
 		public string Name { get; set; }
+		public bool IsSubscribedToNewsletter { get; set; }
+		public MembershipType MembershipType { get; set; }
+		public byte MemberShipTypeId { get; set; }
 
 	}
 }
