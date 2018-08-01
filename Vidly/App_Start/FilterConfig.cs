5e75dd1ff6f9841c5b5405da27132866946d8f83
glenr4 <glenrutherford4@gmail.com>
2018-08-02 08:25:07 +1000

    Changed config to use full IIS so that Authorize Attribute works

--- Vidly/App_Start/FilterConfig.cs
+++ Vidly/App_Start/FilterConfig.cs
@@ -5,6 +5,9 @@ namespace Vidly {
 	public class FilterConfig {
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
 			filters.Add(new HandleErrorAttribute());
+
+			// TODO: this does not work for IISExpress
+			//filters.Add(new AuthorizeAttribute());
 		}
 	}
 }
